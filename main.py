cfe8bd9155c793df6a3a9231cd3a178330a4504d
br-cz <85323237+br-cz@users.noreply.github.com>
2021-12-14 17:52:41 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,55 @@
 import discord
 from discord import guild
+from discord import embeds
+from discord import emoji
+from discord.components import Button
+from discord.ui.view import View
 
 bot = discord.Bot()
+
+class Start(discord.ui.View):
+    def __init__(self):
+        super().__init__()
+        self.value = None
+
+    # When the confirm button is pressed, set the inner value to `True` and
+    # stop the View from listening to more input.
+    # We also send the user an ephemeral message that we're confirming their choice.
+    @discord.ui.button(label="Let's get started!", style=discord.ButtonStyle.green)
+    async def confirm(
+        self, button: discord.ui.Button, interaction: discord.Interaction
+    ):
+        await interaction.response.send_message("Confirming", ephemeral=True)
+        self.value = True
+        self.stop()
+
+    # This one is similar to the confirmation button except sets the inner value to `False`
+    @discord.ui.button(label="Github Repo", style=discord.ButtonStyle.grey)
+    async def cancel(self, button: discord.ui.Button, interaction: discord.Interaction):
+        await interaction.response.send_message("Cancelling", ephemeral=True)
+        self.value = False
+        self.stop()
+  
+@bot.slash_command(guild_ids=[920041129741791294])
+async def start(ctx):    
+    view = Start()
+    
+    embed = discord.Embed(
+        title = "Welcome!",
+        description="I'm Sidney Crosbot, feel free to ask me anything about hockey"
+    )
+    
+    await ctx.respond(embed=embed, view=view)
 
 @bot.slash_command(name="greet", description="get the bot to greet yourself or someone")
 async def hello(ctx, name: str = None):
     name = name or ctx.author.name
     await ctx.respond(f"Hello {name}!")
     
-@bot.slash_command(guild_ids=[920041129741791294], description="get player queries")#name="get player queries", description="used to see all the available player queries")
-async def gpq(ctx):
+@bot.slash_command(guild_ids=[920041129741791294], description="get player queries")
     await ctx.respond("You've chosen to see player queries")
     
-@bot.slash_command(guild_ids=[920041129741791294], description="get coach queries")#name="get player queries", description="used to see all the available player queries")
+@bot.slash_command(guild_ids=[920041129741791294], description="get coach queries")
 async def gcq(ctx):
     await ctx.respond("You've chosen to see coach queries")
     
