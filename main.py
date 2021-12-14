74db0740ed5c3984991a4d98c3f3cbdd39239e77
br-cz <85323237+br-cz@users.noreply.github.com>
2021-12-14 10:05:49 -0600

    Added main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+import discord
+
+bot = discord.Bot()
+
+@bot.slash_command()
+async def hello(ctx, name: str = None):
+    name = name or ctx.author.name
+    await ctx.respond(f"Hello {name}!")
+
+
+bot.run("OTIwMDQ1MTYwMjI0NjU3NDM4.Ybeo0w.XPlqvacAYT2rHPm7OuoKj6Zp1SM")
+
