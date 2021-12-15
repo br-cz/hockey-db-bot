fbe8e174899d209bc9e9dc2b22674057d8776ffc
br-cz <bricz.cruz@gmail.com>
2021-12-15 16:48:35 -0600

    Completed interface outline

--- main.py
+++ main.py
@@ -10,15 +10,70 @@ from discord.colour import CT
 bot = discord.Bot()
 
 #Class storing the view of the different entities
-class ButtonView(discord.ui.View):
+class EntityButtons(discord.ui.View):
     def __init__(self):
         super().__init__(timeout=None)
-        self.add_item(discord.ui.Button(label="Teams", style=discord.ButtonStyle.primary))
-        self.add_item(discord.ui.Button(label="Coaches", style=discord.ButtonStyle.primary))
-        self.add_item(discord.ui.Button(label="Players", style=discord.ButtonStyle.primary))
-        self.add_item(discord.ui.Button(label="Games", style=discord.ButtonStyle.primary))
-        self.add_item(discord.ui.Button(label="Person", style=discord.ButtonStyle.primary))    
+        # self.add_item(discord.ui.Button(label="Teams", style=discord.ButtonStyle.primary))
+        # self.add_item(discord.ui.Button(label="Coaches", style=discord.ButtonStyle.primary))
+        # self.add_item(discord.ui.Button(label="Players", style=discord.ButtonStyle.primary))
+        # self.add_item(discord.ui.Button(label="Games", style=discord.ButtonStyle.primary))
+        # self.add_item(discord.ui.Button(label="Person", style=discord.ButtonStyle.primary))    
+    
+    @discord.ui.button(label="Teams", style=discord.ButtonStyle.primary)
+    async def confirmTeam(self, button: discord.ui.Button, interaction: discord.Interaction):
+        #await interaction.response.send_message("Confirming", ephemeral=True)
+        embed = discord.Embed(
+            title="Please pick the sample query you wish to see!",
+            description="Type the associated slash command to get the query"
+        )
+        embed.add_field(name="/tc", value="This is what the query does", inline=False)
+        embed.add_field(name="/tq", value="This is what the query does", inline=False)
+        await interaction.response.edit_message(embed=embed)
+    
+    @discord.ui.button(label="Coaches", style=discord.ButtonStyle.primary)
+    async def confirmCoach(self, button: discord.ui.Button, interaction: discord.Interaction):
+        #await interaction.response.send_message("Confirming", ephemeral=True)
+        embed = discord.Embed(
+             title="Please pick the sample query you wish to see!",
+            description="Type the associated slash command to get the query"
+        )
+        embed.add_field(name="/cc", value="This is what the query does", inline=False)
+        embed.add_field(name="/cq", value="This is what the query does", inline=False)
+        await interaction.response.edit_message(embed=embed)
         
+    @discord.ui.button(label="Players", style=discord.ButtonStyle.primary)
+    async def confirmPlayer(self, button: discord.ui.Button, interaction: discord.Interaction):
+        #await interaction.response.send_message("Confirming", ephemeral=True)
+        embed = discord.Embed(
+      title="Please pick the sample query you wish to see!",
+            description="Type the associated slash command to get the query"
+        )
+        embed.add_field(name="/pc", value="This is what the query does", inline=False)
+        embed.add_field(name="/pq", value="This is what the query does", inline=False)
+        await interaction.response.edit_message(embed=embed)
+        
+    @discord.ui.button(label="Games", style=discord.ButtonStyle.primary)
+    async def confirmGame(self, button: discord.ui.Button, interaction: discord.Interaction):
+        #await interaction.response.send_message("Confirming", ephemeral=True)
+        embed = discord.Embed(
+             title="Please pick the sample query you wish to see!",
+            description="Type the associated slash command to get the query"
+        )
+        embed.add_field(name="/gc", value="This is what the query does", inline=False)
+        embed.add_field(name="/gq", value="This is what the query does", inline=False)
+        await interaction.response.edit_message(embed=embed)
+        
+    @discord.ui.button(label="Person", style=discord.ButtonStyle.primary)
+    async def confirmPerson(self, button: discord.ui.Button, interaction: discord.Interaction):
+        #await interaction.response.send_message("Confirming", ephemeral=True)
+        embed = discord.Embed(
+          title="Please pick the sample query you wish to see!",
+            description="Type the associated slash command to get the query"
+        )
+        embed.add_field(name="/perc", value="This is what the query does", inline=False)
+        embed.add_field(name="/perq", value="This is what the query does", inline=False)
+        await interaction.response.edit_message(embed=embed)
+    
         
 class Start(discord.ui.View):
     def __init__(self):
@@ -35,12 +90,7 @@ class Start(discord.ui.View):
         embed = discord.Embed(
             title="Which entity do you wish to see the table and queries of?"
         )
-        embed.add_field(name="Teams", value="Field Value", inline="False")
-        embed.add_field(name="Coaches", value="Coaches Value", inline="False")
-        embed.add_field(name="Players", value="Players Value", inline="False")
-        embed.add_field(name="Games", value="Games Value", inline="False")
-        embed.add_field(name="Person", value="Person Value", inline="False")
-        await interaction.response.edit_message(view=ButtonView(), embed=embed)
+        await interaction.response.edit_message(view=EntityButtons(), embed=embed)
         
         
     
@@ -56,8 +106,7 @@ async def start(ctx):
     
     await ctx.respond(embed=embed, view=view)
     await view.wait()
-   
-   # await ctx.respond(embed=embed,view=view)
+
    
   
 
@@ -75,7 +124,7 @@ async def start(ctx):
 
 
 
-
+#Queries for later
 @bot.slash_command(name="greet", description="get the bot to greet yourself or someone")
 async def hello(ctx, name: str = None):
     name = name or ctx.author.name
@@ -94,4 +143,6 @@ async def dt(ctx, numteam: int):
     numteam = numteam
     await ctx.respond(f"Find the names of all players who have played in at least {numteam} different teams")
     
+    
+
 bot.run("OTIwMDQ1MTYwMjI0NjU3NDM4.Ybeo0w.XPlqvacAYT2rHPm7OuoKj6Zp1SM")
